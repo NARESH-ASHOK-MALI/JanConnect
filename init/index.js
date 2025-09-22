da9da6ca4f2c637bddc948c0e806352a575b6dec
NARESH ASHOK MALI <nareshashokmali@gmail.com>
2025-09-22 20:53:54 +0530

    JanConnect v_1.0

---
+++ init/index.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/JanConnect";
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  console.log("existing data deleted");
+  await Listing.insertMany(initData);
+  console.log("data was initialized");
+};
+
+initDB();
---
+++ models/listing.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default:
+      "https://images.squarespace-cdn.com/content/v1/573365789f726693272dc91a/1704992146415-CI272VYXPALWT52IGLUB/AdobeStock_201419293.jpeg?format=1500w",
+    set: (v) =>
+      v === ""
+        ? "https://images.squarespace-cdn.com/content/v1/573365789f726693272dc91a/1704992146415-CI272VYXPALWT52IGLUB/AdobeStock_201419293.jpeg?format=1500w"
+        : v,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  location: String,
+  city: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
